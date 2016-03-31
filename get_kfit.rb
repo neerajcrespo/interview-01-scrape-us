47c77e02cd5f28327b3579c70d7972c5bfb2a94d
peiyee <tehpeiyee_1989@hotmail.com>
2016-03-31 12:22:56 +0800

    able to get name, address, city, lat,lng

--- get_kfit.rb
+++ get_kfit.rb
@@ -1,37 +1,64 @@
 require 'pry'
 require 'net/http'
 require 'nokogiri'
-require 'json'
 uri = 'https://access.kfit.com/schedules/614474'
 partner = 'https://access.kfit.com/partners/517'
-# content = "var outlet_details = {\n  id:          '764',\n  company_id:  '616',\n  name:        'The Club@Bukit Utama (Gym Access)',\n  address:     '1, Club Drive, Bukit Utama, Bandar Utama, 47800 Petaling Jaya',\n  city:        'kuala-lumpur',\n  position:    new google.maps.LatLng('3.15234', '101.602845'),\n  icon:        \"https://d2wwlnsocmwlmh.cloudfront.net/assets/kfit-marker-dc239a05fd85cf77e9aaa4731cda29ec8c965e35ae811394fc2321f97c2050ee.png\"\n};\n\n$(function(){\n  OutletMap.initialize(outlet_details);\n  $(window).off('.affix');\n  $(\"body\")\n    .removeClass(\"affix affix-top affix-bottom\")\n    .removeData(\"bs.affix\");\n});\n\nvar MAX_DISTANCE = 300;\nvar draggable = $('div#draggable');\n\nDraggable.create(draggable, {\n  onDrag:function(e) {\n    var x = this.target._gsTransform.x,\n        y = this.target._gsTransform.y,\n        distance = Math.sqrt(x * x + y * y);\n\n    if (distance > MAX_DISTANCE) {\n       this.endDrag(e);\n    }\n  },\n  onDragEnd:function() {\n    TweenMax.to(draggable, 1, {x:0, y:0, ease:Elastic.easeOut});\n  }\n});\n\n\n$(window).scroll(function() { \n    if ($(window).scrollTop() >= $(\".activity-image\").offset().top) {  \n      $(\".floating-bar\").addClass(\"reveal\");\n    } else if ($(window).scrollTop() < $(\".activity-image\").offset().top) {\n      $(\".floating-bar\").removeClass(\"reveal\");\n    } \n});"
 
-# pattern = /var outlet_details\s*=\s*{.+};/m
-
-# data = content.scan(pattern).first
-
-# lala = data[(data.index('{')+1)..(data.rindex('};'))].strip
 def get_script_node
+	puts "searching script nodes..."
+	partner = 'https://access.kfit.com/partners/517'
+	response = fetch(partner)
+	page = Nokogiri::HTML(response.body)
+	node = page.xpath('//script[contains(text(),"var outlet_details")]')
+	if node.empty?
+		warn("cannot find var outlet_details in script nodes")
+	else
+		node.first.text
+	end
 end
 
-def get_out_details_string
+def get_out_details(details_str)
+	name,address,city = ""
+	lat,lng = nil
+	removed_var_name = details_str[(details_str.index('{')+1)..(details_str.rindex('};')-1)].strip
+	rows = removed_var_name.split(",\n").map(&:strip)
+	rows.map!{|r| /(.+):(.+)/.match(r).to_a.map(&:strip)}
+	rows.each do |r|
+		case r[1]
+		when "name" then
+			name = get_name(r[2])
+		when "address" then
+			address = get_address(r[2])
+		when "city" then
+			city = get_city(r[2])
+		when "position" then
+			lat,lng = get_latlng(r[2])
+		else
+		end
+	end
+	return name,address,city,lat,lng
 end
 
-def get_address
+def get_name(str)
+	str.gsub(/\A'|'\Z/, '')
 end
 
-def get_city
+def get_address(str)
+	str.gsub(/\A'|'\Z/, '')
 end
 
-def get_latlng
+def get_city(str)
+	city = str.gsub(/\A'|'\Z/, '')
+	city = city.gsub(/\W/,' ').split.map(&:capitalize).join(' ')
+end
+
+def get_latlng(str)
+	pattern = /\d+.\d+/
+	str.scan(pattern).map(&:to_f)
 end
 
 def get_partner_details
-	get_script_node
-	get_out_details_string
-	get_address
-	get_city
-	get_latlng
+	name,address,city,lat,lng = get_out_details(get_script_node)
 end
 
 def fetch(uri_string)
@@ -46,6 +73,7 @@ def fetch(uri_string)
 		nil
 	end
 end
+get_partner_details
 # if response = fetch(uri)
 # 	page = Nokogiri::HTML(response.body)
 # 	binding.pry
