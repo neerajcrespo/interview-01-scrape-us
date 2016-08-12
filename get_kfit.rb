d19887a6572dce10387f6f8cdc9cceb971b71ab8
neerajcrespo <neerajcrespo@gmail.com>
2016-08-12 23:38:36 +0530

    unzip address city lat long from scrip xpath

--- get_kfit.rb
+++ get_kfit.rb
@@ -6,19 +6,45 @@ BASE_URL = "https://access.kfit.com"
 $mechanize = Mechanize.new #let it act like a singleton
 
 def get_address_and_phone_string_from_partner_page(page) 
-  address_data, phone_data = ""
+  city, partner_name, address, latitude, longitude, phone_number = ""
   #<ul class="list-unstyled studio-details"><li><span>Address</span><p>Unit 28 &amp; 30 - 2, Plaza Damansara, Jalan Medan Setia 2, Bukit Damansara, 50490 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur</p></li><li><span>Activities</span><p>Fat Lose in 60, SpartanXFit, XFit</p></li></ul></div></div><script type="text/javascript">var outlet_details
-  if page 
-    address_data = page.parser.xpath('//script[contains(text(),"var outlet_details")]').first.children.first.to_html
-    phone_data = scrap_phone_from_page(page)
-  else
-    puts "No Data or Page 404"
-    return nil
+  xpath_data = page.parser.xpath('//script[contains(text(),"var outlet_details")]').first.children.first.to_html
+  if xpath_data
+    parsed_data = Hash.new
+    parsed_data =  sanitize_xpath(xpath_data)
+    phone_number = scrap_phone_from_page(page)
+    rating = get_rating(page)
   end
-  return address_data, phone_data
+  
+
+  return city, partner_name, address, latitude, longitude, phone_number
 end
 
-def extract_row_from_address_xpath_string(address_string)
+def sanitize_xpath(xpath_data)
+  if address_string
+    unzipped = address_string[(address_string.index('{')+1)..(address_string.index('};')-1)].strip.split(",\n").map(&:strip)
+    data_hash = Hash.new
+    unzipped.each do|field|
+      key = field.first.to_s.strip
+      value = field.last.to_s.strip
+      case key
+      when 'name'
+        data_hash['name'] = value
+      when 'address'
+        data_hash['address'] = value
+      when 'city'
+        data_hash['city'] = value
+      when 'position'
+        data_hash['latitude'], data_hash['longitude'] = value[value.index('(')..value.index[')']].split(',').map(&:to_s)
+      end
+    end
+    return data_hash
+  end
+end
+
+
+def get_rating(page)
+
 
 end
 
@@ -43,12 +69,13 @@ def get_link_page(page)
 end
 
 
-def id_to_csv_row_builder(id)
+def id_to_csv_row(id)
   partner_url = BASE_URL.to_s+"/partners/#{id}"
+  city, partner_name, address, latitude, longitude, average_rating, phone_number = ""
   begin 
     page = $mechanize.get(partner_url)
     if page
-      puts "page exist #{id}"
+      city, partner_name, address, latitude, longitude = get_address_and_phone_string_from_partner_page(page)
     else
       puts "page No exist with this resource id"
     end
@@ -59,9 +86,8 @@ end
 
 #add = get_address_and_phone_string_from_partner_page("https://access.kfit.com/partners/5")
 #phone = get_phone_number_from_partner_page(url)
-for i in (1..10)
-  id_to_csv_row_builder(i)
-end
+
+id_to_csv_row(5)
 binding.pry
 puts "end"
 
